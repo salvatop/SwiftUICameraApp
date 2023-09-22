cd05903808897fc4e6aa6f119e3a02b34a08adcc
Salvatore Palazzo <salvatop78@gmail.com>
2023-09-22 13:12:47 -0400

    Initial Commit

---
+++ SwiftUICameraApp/SwiftUICameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICameraAppApp.swift
+//  SwiftUICameraApp
+//
+//  Created by little-ac on 2023-09-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
